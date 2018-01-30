aa05e7cf5ad79b706462c683bb45ec264cb9d47f
Camille Mauban <camille.mauban@gmail.com>
2018-01-29 21:36:00 -0500

    completed 3.11

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,12 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
+
+  event.respondWith(
+    new Response('<h1 class="a-winner-is-me">yay me</h1>', {
+      headers: {'Content-Type':'text/html'}
+    })
+  );
+
   console.log(event.request);
 });
