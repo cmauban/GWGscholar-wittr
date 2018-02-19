2edd1a8e0e2977ca4d92eb472f5b3706e475eb9c
Camille Mauban <camille.mauban@gmail.com>
2018-02-19 14:15:56 -0500

    copied from local machine to this remote branch.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,30 @@
-self.addEventListener('fetch', function(event) {
-  // tells the browser we're going to handle it ourselves
-  event.respondWith(
-    new Response('Hello <b>World</b>', {// first param -
-      headers: {'foo': 'bar'} // second param - object
+self.addEventListener('install', function(event) {
+  var urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
+
+  event.waitUntil(
+    // TODO: open a cache named 'wittr-static-v1'
+    caches.open('wittr-static-v1').then(function(cache){
+      // Add cache the urls from urlsToCache
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
     })
   );
+});
 
+self.addEventListener('fetch', function(event) {
+  // Leave this blank for now.
+  // We'll get to this in the next task.
 });
